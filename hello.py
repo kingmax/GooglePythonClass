2f363afba9c877b7c2237948b2956d97c342576f
iJasonLee <kingmax_res@163.com>
2017-07-08 22:05:08 +0800

    add comments and docstring

--- hello.py
+++ hello.py
@@ -5,13 +5,19 @@
   Purpose: 
   Created: 2017/7/8
 """
-
+#import modules used here (sys is a very standard one)
 import sys
 
+#Gather our code in a main() function
 def main():
+    '''
+    there is docstring
+    about main function
+    '''
     print('Hello, there %s'%sys.argv[1])
     
 def usage():
+    '''how to usage this module'''
     print("""
     ------- USAGE -------
      hello.py [your_name]
