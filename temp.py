ae290c694533076a98497e937062e163f47c572c
liyajun <liyajun@Z87-008.sz.xpec.com.cn>
2017-07-10 11:16:44 +0800

    temp msg

---
+++ temp.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#coding:utf-8
+"""
+  Author:  JasonLee --<>
+  Purpose: testing
+  Created: 2017/7/10
+"""
+
+import sys
+
+def print_sys_path():
+    for p in sys.path:
+        print(p)
+
+def haha():
+    print('haha')
+
+def main():
+    print_sys_path()
+    haha()
+
+
+if __name__ == '__main__' :
+    main()
